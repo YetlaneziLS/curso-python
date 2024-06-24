62243b673c32ea3734237443b436683b8fcc81df
Yet Linares <yetlinares@gmail.com>
2024-06-23 21:31:04 -0600

    Op comparación

---
+++ operadores/op_comparacion.py
@@ -0,0 +1,15 @@
+# Los operadores de comparación nos devuelve un boolean
+
+igual_que = 5 == 6
+
+distinsto_de = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5 >= 6
+
+menor_o_igual = 5 <= 6
+
+print(igual_que)  # Esto arrojará un false
